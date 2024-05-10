df8302dbd0b868587cb696d28a10fd1dc71ecdcc
Jonas Pereira <66091286+jonasdspereira@users.noreply.github.com>
2024-05-10 14:54:44 +0000

    Endpoint Read By ID '[GET] /personagem/:id' criado

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ app.get ('/personagem', function (req, res) {
   res.send(lista)
 })
 
+// Endpoint Read By ID [GET] /personagem/:id
+
+app.get('/personagem/:id', function (req, res) {
+  // Acessamos o parâmetro de rota ID
+  const id = req.params.id
+
+  // Acessa o item na lista usando o ID - 1
+  const item  = lista[id - 1]
+  res.send(item)
+})
+
 app.listen(3000, function() {
   console.log("Aplicação rodando em http://localhost:3000")
 })
