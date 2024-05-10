5d5f1d9bf3a48068881cadc55869238c81248dff
Jonas Pereira <66091286+jonasdspereira@users.noreply.github.com>
2024-05-10 14:47:06 +0000

    Endpoint 'Read All [GET] /personagem' implementado

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+const lista = ['Java', 'Kotlin', 'Android']
+// Endpoint Read All [GET] /personagem
+app.get ('/personagem', function (req, res) {
+  res.send(lista)
+})
+
 app.listen(3000, function() {
   console.log("Aplicação rodando em http://localhost:3000")
 })
